3d68f20516e53a028e93295f24433361c9ac892d
dmitryame <dmitry@custommode.com>
2017-10-20 21:48:37 -0400

    saving image into galery

--- Echowaves/HomeViewController.swift
+++ Echowaves/HomeViewController.swift
@@ -10,62 +10,100 @@ import UIKit
 
 class HomeViewController:
     UIViewController,
-//    UICollectionViewDataSource,
+    //    UICollectionViewDataSource,
     UICollectionViewDelegate,
     UIImagePickerControllerDelegate,
     UINavigationControllerDelegate {
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-         picker.delegate = self
+        picker.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-  
+    
+    
     @IBOutlet weak var collectionview: UICollectionView!
-
     
     
     
     
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//
+    
+    //
+    //    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    //        <#code#>
+    //    }
+    //
+    //    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    //        <#code#>
+    //    }
+    //
     
     let picker = UIImagePickerController()
     
     @IBAction func openCameraButtonClicked(_ sender: Any) {
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            
-            picker.delegate = self
-            picker.sourceType = .camera;
-            picker.allowsEditing = false
-            self.present(picker, animated: true, completion: nil)
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                
+                picker.delegate = self
+                picker.sourceType = .camera;
+                picker.allowsEditing = false
+                self.present(picker, animated: true, completion: nil)
+            } else {
+                noCamera()
+            }
         }
     }
     
+    func noCamera(){
+        let alertVC = UIAlertController(
+            title: "No Camera",
+            message: "Sorry, this device has no camera",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
+    
     //MARK: - Delegates
-    private func imagePickerController(_ picker: UIImagePickerController,
-                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any])
     {
         //this is where we have to store locally and upload the photo
-        
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        UIImageWriteToSavedPhotosAlbum(chosenImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
+    
+    //MARK: - Add image to Library
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        dismiss(animated: true, completion: nil)
+
+        if let error = error {
+            // we got back an error!
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
     
 }
 
